c622978f99dba9f8ef2682132cca2aa2773bd730
fahad114433 <fahadsaeed114433@gmail.com>
2026-02-20 14:05:20 +0600

    update

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from "./routes/authRoute.js"; 
+import resumeRoutes from "./routes/resumeRoute.js";
+
+dotenv.config();
+
+const app = express();
+
+
+app.use(cors({
+
+}));
+
+
+app.use(express.json());;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Atlas Connected"))
+  .catch((err) => console.log(err));
+
+
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/resume", resumeRoutes);
+
+
+export default app;
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api", 
+  baseURL: "https://resume-n22h-4fl3tx2ea-fahad114433s-projects.vercel.app", 
 });
 
 // Add token automatically
