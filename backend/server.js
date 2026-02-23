dfef390f8acd8a4fdb1c7c4b07cdc405dd2dcc68
fahad114433 <fahadsaeed114433@gmail.com>
2026-02-23 13:25:24 +0600

    update

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 
-app.use("/auth", authRoutes);
-app.use("/resume", resumeRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/resume", resumeRoutes);
 
 
 const PORT = process.env.PORT || 5000;
