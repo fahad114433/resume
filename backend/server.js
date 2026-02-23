9cbc880a34035df5a8271f2d18a6b016e5a82512
fahad114433 <fahadsaeed114433@gmail.com>
2026-02-23 13:06:33 +0600

    update

--- backend/server.js
+++ backend/server.js
@@ -9,20 +9,8 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = ["https://resume-six-plum-53.vercel.app"];
+app.use(cors());
 
-app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true); // allow REST clients like Postman
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true,
-  methods: ["GET","POST","PUT","DELETE","OPTIONS"]
-}));
 app.use(express.json());;
 
 mongoose.connect(process.env.MONGO_URI)
@@ -32,9 +20,9 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 
-app.use("/api/auth", authRoutes);
-app.use("/api/resume", resumeRoutes);
+app.use("/auth", authRoutes);
+app.use("/resume", resumeRoutes);
+
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api"
+  baseURL: "/api",
 });
 
 // Add token automatically
